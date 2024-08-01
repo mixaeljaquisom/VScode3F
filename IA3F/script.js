8e1e0f5980decfc5fd0bb31346ffa1945e25ab12
mixaeljaquisom <141679676+mixaeljaquisom@users.noreply.github.com>
2024-08-01 16:47:57 -0300

    Add files via upload

---
+++ IA3F/script.js
@@ -0,0 +1,4 @@
+const  caixaPrincipal = document.querySelector('.caixa-principal');
+const  caixaPerguntas = document.querySelector('.caixa-Perguntas');
+const  caixaAlternativas = document.querySelector('.caixa-Alternativas');
+const  caixaResultado = document.querySelector('.caixa-Resultado');
